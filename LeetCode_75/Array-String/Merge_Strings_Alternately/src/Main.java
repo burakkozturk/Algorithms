35fa373cdce73829e58f352f46042fc97686addf
burak <devx.burak@gmail.com>
2024-12-06 16:25:57 +0300

    LeetCode -1768

---
+++ LeetCode_75/Array-String/Merge_Strings_Alternately/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // LeetCode 75 -------> 1768
+
+        Solution solution = new Solution();
+        String result = solution.mergeAlternately("abcd","qwer");
+
+        System.out.print(result);
+    }
+}
